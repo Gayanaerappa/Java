a4031b54af5fe33ff4bd22d41263e92577bcc331
Gayana Erappa <gayanaerappa2027@gmail.com>
2026-02-17 22:45:14 +0530

    Create LargestInArray.java

---
+++ LargestInArray.java
@@ -0,0 +1,16 @@
+ublic class LargestInArray {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 45, 23, 89, 5};
+
+        int largest = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+        }
+
+        System.out.println("Largest Element: " + largest);
+    }
+}
